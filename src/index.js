46f0bcff93b8c17f2ed13e29bdc1f2dedf522509
LomaxRx <andrewlomaxr@gmail.com>
2018-09-04 12:44:50 -0400

    method for handling multiple charts with a single data source

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import './index.scss';
 
 
-window.renderDataViz = function(el){
+let settings = {
 
+
+};
+
+let queue = [];
+let data = null;
+
+fetch('endpoint').then(response => response.json()).then((_data)=>{
+  data = _data;
+  for(let i=0; i<queue.length; i++)
+    queue[i].render(data);
+});
+
+window.renderDataViz = function(el){
+  let id = el.getAttribute('id');
+  let chart = settings[id];
+  if(!chart) return;
+
+  if(data){
+    chart.render(data);
+  } else {
+    queue.push(chart);
+  }
 }
