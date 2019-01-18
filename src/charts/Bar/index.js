d69b256b655679283f8db1f8bff4d4cc3e802f65
lorenries <lorenries@users.noreply.github.com>
2019-01-18 12:30:36 -0500

    add bar chart

---
+++ src/charts/Bar/index.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { Bar } from "@vx/shape";
+import { Group } from "@vx/group";
+import { AxisBottom, AxisLeft } from "@vx/axis";
+import { scaleBand, scaleLinear } from "@vx/scale";
+import { GridRows } from "@vx/grid";
+import { max } from "d3-array";
+import Chart from "../Chart";
+
+export default ({
+  maxWidth,
+  height,
+  data,
+  x,
+  y,
+  renderTooltip,
+  xFormat,
+  yFormat,
+  xAxisLabel,
+  yAxisLabel,
+  numTicksY = 5,
+  color = "#22C8A3",
+  margin = {
+    top: 10,
+    left: 60,
+    right: 10,
+    bottom: 50
+  }
+}) => {
+  return (
+    <Chart maxWidth={maxWidth} height={height} renderTooltip={renderTooltip}>
+      {({ width, height, handleMouseEnter, handleMouseLeave }) => {
+        const xMax = width - margin.left - margin.right;
+        const yMax = height - margin.top - margin.bottom;
+
+        const xScale = scaleBand({
+          rangeRound: [0, xMax],
+          domain: data.map(x),
+          padding: 0.2
+        });
+
+        const yScale = scaleLinear({
+          rangeRound: [yMax, 0],
+          domain: [0, max(data, y)]
+        });
+
+        return (
+          <Group top={margin.top} left={margin.left}>
+            <GridRows scale={yScale} width={xMax} numTicks={numTicksY} />
+            <Group>
+              {data.map((datum, i) => {
+                return (
+                  <Bar
+                    key={`bar-${i}`}
+                    x={xScale(x(datum))}
+                    y={yScale(y(datum))}
+                    width={xScale.bandwidth()}
+                    height={yMax - yScale(y(datum))}
+                    fill={color}
+                    onMouseMove={event =>
+                      handleMouseEnter({ event, data, datum })
+                    }
+                    onMouseLeave={handleMouseLeave}
+                  />
+                );
+              })}
+            </Group>
+            <AxisLeft
+              scale={yScale}
+              label={yAxisLabel}
+              labelProps={{
+                textAnchor: "middle"
+              }}
+              hideTicks={true}
+              hideAxisLine={true}
+              numTicks={numTicksY}
+              tickFormat={yFormat}
+              tickLabelProps={() => ({
+                textAnchor: "end",
+                verticalAnchor: "middle"
+              })}
+            />
+            <AxisBottom
+              top={yMax}
+              scale={xScale}
+              label={xAxisLabel}
+              hideAxisLine={false}
+              hideTicks={false}
+              tickFormat={xFormat}
+              tickLabelProps={() => ({
+                textAnchor: "middle",
+                width: xScale.bandwidth(),
+                verticalAnchor: "end",
+                dy: "0.2em"
+              })}
+              labelProps={{
+                dy: "3em",
+                textAnchor: "middle",
+                y: 0
+              }}
+            />
+          </Group>
+        );
+      }}
+    </Chart>
+  );
+};
