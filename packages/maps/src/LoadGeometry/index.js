1152acd53a1976357fdeb0d5622edb8783dee0a5
Loren Riesenfeld <lorenries@users.noreply.github.com>
2019-01-20 17:16:22 -0500

    [maps] add LoadGeometry helper component

---
+++ packages/maps/src/LoadGeometry/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { feature } from "topojson-client";
+
+export default class LoadGeometry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feature: null
+    };
+  }
+  async componentDidMount() {
+    if (this.props.geometry === "world") {
+      const response = await fetch(
+        "https://s3-us-west-2.amazonaws.com/na-data-projects/geography/world.json"
+      );
+      const world = await response.json();
+      this.setState({
+        feature: feature(world, world.objects.countries)
+      });
+    } else if (this.props.geometry === "us") {
+      const response = await fetch(
+        "https://s3-us-west-2.amazonaws.com/na-data-projects/geography/us.json"
+      );
+      const us = await response.json();
+      this.setState({
+        feature: feature(us, us.objects.states)
+      });
+    }
+  }
+  render() {
+    if (!this.state.feature) return;
+    return this.props.children(this.state.feature);
+  }
+}
