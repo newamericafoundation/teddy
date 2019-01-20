3bbb340e63a6bd55c35064d4646f242eaf9aa1a2
Loren Riesenfeld <lorenries@users.noreply.github.com>
2019-01-20 17:15:16 -0500

    [maps] add Cartogram

---
+++ packages/maps/src/Cartogram/index.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Chart } from "@newamerica/charts";
+import { Text } from "@vx/text";
+import { scaleLinear, scaleQuantize } from "@vx/scale";
+import { quantize, interpolateRgb } from "d3-interpolate";
+import { max, extent } from "d3-array";
+import { map } from "d3-collection";
+import layout from "./layout";
+import "./Cartogram.scss";
+
+/**
+ * Cartogram map component
+ * TODO: legend and margins
+ */
+const Cartogram = ({
+  maxWidth,
+  data,
+  renderTooltip,
+  valueAccessor,
+  idAccessor = d => d.id,
+  mapStroke = "#fff",
+  mapFill = "#cbcbcd",
+  colors = ["#e5f5f9", "#2ca25f"],
+  numStops = 6
+}) => {
+  const dataMap = map(data, idAccessor);
+  const colorArray = quantize(interpolateRgb(colors[0], colors[1]), numStops);
+  const colorScale = scaleQuantize({
+    domain: extent(data, valueAccessor),
+    range: colorArray
+  });
+  const x = d => d.x;
+  const y = d => d.y;
+  const boxesWide = max(layout, x) + 1;
+  const boxesTall = max(layout, y) + 1;
+  const ratio = boxesTall / boxesWide;
+  return (
+    <Chart
+      maxWidth={maxWidth}
+      aspectRatio={ratio}
+      renderTooltip={renderTooltip}
+    >
+      {({ width, height, handleMouseEnter, handleMouseLeave }) => {
+        if (width < 10) return;
+        const boxWidth = width / boxesWide;
+        const textOffset = boxWidth / 2;
+        const xScale = scaleLinear({
+          domain: [0, boxesWide],
+          range: [0, width]
+        });
+        const yScale = scaleLinear({
+          domain: [0, boxesTall],
+          range: [0, height]
+        });
+        return (
+          <g>
+            {layout.map((state, i) => {
+              const xPos = xScale(x(state));
+              const yPos = yScale(y(state));
+              const datum = dataMap.get(state.fips);
+              return (
+                <g
+                  onMouseMove={event => handleMouseEnter({ event, datum })}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <rect
+                    x={xPos}
+                    y={yPos}
+                    width={boxWidth}
+                    height={boxWidth}
+                    fill={datum ? colorScale(valueAccessor(datum)) : mapFill}
+                    stroke={mapStroke}
+                  />
+                  <Text
+                    x={xPos + textOffset}
+                    y={yPos + textOffset}
+                    textAnchor="middle"
+                    verticalAnchor="middle"
+                    className="dv-Cartogram__label"
+                  >
+                    {width < 640 ? state.short : state.abb}
+                  </Text>
+                </g>
+              );
+            })}
+          </g>
+        );
+      }}
+    </Chart>
+  );
+};
+
+export default Cartogram;
---
+++ packages/maps/src/Cartogram/layout.js
@@ -0,0 +1,412 @@
+const layout = [
+  {
+    x: 0,
+    y: 0,
+    short: "AK",
+    long: "Alaska",
+    abb: "Alaska",
+    fips: "02"
+  },
+  {
+    x: 10,
+    y: 0,
+    short: "ME",
+    long: "Maine",
+    abb: "Maine",
+    fips: "23"
+  },
+  {
+    x: 9,
+    y: 1,
+    short: "VT",
+    long: "Vermont",
+    abb: "Vt.",
+    fips: "50"
+  },
+  {
+    x: 10,
+    y: 1,
+    short: "NH",
+    long: "New Hampshire",
+    abb: "N.H.",
+    fips: "33"
+  },
+  {
+    x: 0,
+    y: 2,
+    short: "WA",
+    long: "Washington",
+    abb: "Wash.",
+    fips: "53"
+  },
+  {
+    x: 1,
+    y: 2,
+    short: "ID",
+    long: "Idaho",
+    abb: "Idaho",
+    fips: "16"
+  },
+  {
+    x: 2,
+    y: 2,
+    short: "MT",
+    long: "Montana",
+    abb: "Mont.",
+    fips: "30"
+  },
+  {
+    x: 3,
+    y: 2,
+    short: "ND",
+    long: "North Dakota",
+    abb: "N.D.",
+    fips: "38"
+  },
+  {
+    x: 4,
+    y: 2,
+    short: "MN",
+    long: "Minnesota",
+    abb: "Minn.",
+    fips: "27"
+  },
+  {
+    x: 6,
+    y: 2,
+    short: "MI",
+    long: "Michigan",
+    abb: "Mich.",
+    fips: "26"
+  },
+  {
+    x: 8,
+    y: 2,
+    short: "NY",
+    long: "New York",
+    abb: "N.Y.",
+    fips: "36"
+  },
+  {
+    x: 9,
+    y: 2,
+    short: "MA",
+    long: "Massachusetts",
+    abb: "Mass.",
+    fips: "25"
+  },
+  {
+    x: 10,
+    y: 2,
+    short: "RI",
+    long: "Rhode Island",
+    abb: "R.I.",
+    fips: "44"
+  },
+  {
+    x: 0,
+    y: 3,
+    short: "OR",
+    long: "Oregon",
+    abb: "Ore.",
+    fips: "41"
+  },
+  {
+    x: 1,
+    y: 3,
+    short: "UT",
+    long: "Utah",
+    abb: "Utah",
+    fips: "49"
+  },
+  {
+    x: 2,
+    y: 3,
+    short: "WY",
+    long: "Wyoming",
+    abb: "Wyo.",
+    fips: "56"
+  },
+  {
+    x: 3,
+    y: 3,
+    short: "SD",
+    long: "South Dakota",
+    abb: "S.D.",
+    fips: "46"
+  },
+  {
+    x: 4,
+    y: 3,
+    short: "IA",
+    long: "Iowa",
+    abb: "Iowa",
+    fips: "19"
+  },
+  {
+    x: 5,
+    y: 3,
+    short: "WI",
+    long: "Wisconsin",
+    abb: "Wis.",
+    fips: "55"
+  },
+  {
+    x: 6,
+    y: 3,
+    short: "IN",
+    long: "Indiana",
+    abb: "Ind.",
+    fips: "18"
+  },
+  {
+    x: 7,
+    y: 3,
+    short: "OH",
+    long: "Ohio",
+    abb: "Ohio",
+    fips: "39"
+  },
+  {
+    x: 8,
+    y: 3,
+    short: "PA",
+    long: "Pennsylvania",
+    abb: "Pa.",
+    fips: "42"
+  },
+  {
+    x: 9,
+    y: 3,
+    short: "NJ",
+    long: "New Jersey",
+    abb: "N.J.",
+    fips: "34"
+  },
+  {
+    x: 10,
+    y: 3,
+    short: "CT",
+    long: "Connecticut",
+    abb: "Conn.",
+    fips: "09"
+  },
+  {
+    x: 0,
+    y: 4,
+    short: "CA",
+    long: "California",
+    abb: "Calif.",
+    fips: "06"
+  },
+  {
+    x: 1,
+    y: 4,
+    short: "NV",
+    long: "Nevada",
+    abb: "Nev.",
+    fips: "32"
+  },
+  {
+    x: 2,
+    y: 4,
+    short: "CO",
+    long: "Colorado",
+    abb: "Colo.",
+    fips: "08"
+  },
+  {
+    x: 3,
+    y: 4,
+    short: "NE",
+    long: "Nebraska",
+    abb: "Neb.",
+    fips: "31"
+  },
+  {
+    x: 4,
+    y: 4,
+    short: "MO",
+    long: "Missouri",
+    abb: "Mo.",
+    fips: "29"
+  },
+  {
+    x: 5,
+    y: 4,
+    short: "IL",
+    long: "Illinois",
+    abb: "Ill.",
+    fips: "17"
+  },
+  {
+    x: 6,
+    y: 4,
+    short: "KY",
+    long: "Kentucky",
+    abb: "Ky.",
+    fips: "21"
+  },
+  {
+    x: 7,
+    y: 4,
+    short: "WV",
+    long: "West Virginia",
+    abb: "W.Va.",
+    fips: "54"
+  },
+  {
+    x: 8,
+    y: 4,
+    short: "VA",
+    long: "Virginia",
+    abb: "Va.",
+    fips: "51"
+  },
+  {
+    x: 9,
+    y: 4,
+    short: "MD",
+    long: "Maryland",
+    abb: "Md.",
+    fips: "24"
+  },
+  {
+    x: 10,
+    y: 4,
+    short: "DE",
+    long: "Delaware",
+    abb: "Del.",
+    fips: "10"
+  },
+  {
+    x: 1,
+    y: 5,
+    short: "AZ",
+    long: "Arizona",
+    abb: "Ariz.",
+    fips: "04"
+  },
+  {
+    x: 2,
+    y: 5,
+    short: "NM",
+    long: "New Mexico",
+    abb: "N.M.",
+    fips: "35"
+  },
+  {
+    x: 3,
+    y: 5,
+    short: "KS",
+    long: "Kansas",
+    abb: "Kan.",
+    fips: "20"
+  },
+  {
+    x: 4,
+    y: 5,
+    short: "AR",
+    long: "Arkansas",
+    abb: "Ark.",
+    fips: "05"
+  },
+  {
+    x: 5,
+    y: 5,
+    short: "TN",
+    long: "Tennessee",
+    abb: "Tenn.",
+    fips: "47"
+  },
+  {
+    x: 6,
+    y: 5,
+    short: "NC",
+    long: "North Carolina",
+    abb: "N.C.",
+    fips: "37"
+  },
+  {
+    x: 7,
+    y: 5,
+    short: "SC",
+    long: "South Carolina",
+    abb: "S.C.",
+    fips: "45"
+  },
+  {
+    x: 8,
+    y: 5,
+    short: "DC",
+    long: "District of Columbia",
+    abb: "D.C.",
+    fips: "11"
+  },
+  {
+    x: 3,
+    y: 6,
+    short: "OK",
+    long: "Oklahoma",
+    abb: "Okla.",
+    fips: "40"
+  },
+  {
+    x: 4,
+    y: 6,
+    short: "LA",
+    long: "Louisiana",
+    abb: "La.",
+    fips: "22"
+  },
+  {
+    x: 5,
+    y: 6,
+    short: "MS",
+    long: "Mississippi",
+    abb: "Miss.",
+    fips: "28"
+  },
+  {
+    x: 6,
+    y: 6,
+    short: "AL",
+    long: "Alabama",
+    abb: "Ala.",
+    fips: "01"
+  },
+  {
+    x: 7,
+    y: 6,
+    short: "GA",
+    long: "Georgia",
+    abb: "Ga.",
+    fips: "13"
+  },
+  {
+    x: 0,
+    y: 7,
+    short: "HI",
+    long: "Hawaii",
+    abb: "Hawaii",
+    fips: "15"
+  },
+  {
+    x: 3,
+    y: 7,
+    short: "TX",
+    long: "Texas",
+    abb: "Texas",
+    fips: "48"
+  },
+  {
+    x: 8,
+    y: 7,
+    short: "FL",
+    long: "Florida",
+    abb: "Fla.",
+    fips: "12"
+  }
+];
+
+export default layout;
